ed7e52604da554b26ceb61646aa24ef64bae78ea
Barış Sokat <barissokat@outlook.com>
2021-12-20 14:37:53 +0300

    Add child-to-parent component communication

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App () {
       amount: 94.12,
       date: new Date(2020, 7, 14)
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -26,9 +31,14 @@ function App () {
     }
   ]
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log({ ...data, expense })
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense expenses={data} onAddExpense={addExpenseHandler} />
       <Expenses expenses={data} />
     </div>
   )
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
@@ -48,7 +48,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     }
 
-    console.log(expenseData)
+    props.onSaveExpenseData(expenseData)
     setEnteredTitle('')
     setEnteredAmount('')
     setEnteredDate('')
@@ -67,7 +67,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
-            value={enteredAmount} 
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -77,7 +77,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
-            value={enteredDate} 
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
